200b34a03acc29740862ff1bb703a6171e9feb5e
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2019-01-14 18:07:44 +0600

    Redux connect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; // ---1-й шаг - импорт connect из react-redux теперь - экспортируется connect компонент 
 
 class App extends Component {
   state = { count: 0};
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
+
+    //---React--- this.setState({ count: this.state.count + 1 });
   };
   decrement = () =>{
-    this.setState({
-      count: this.state.count - 1
-    });
+    //---React--- this.setState({ count: this.state.count - 1 });
   };
 
   render() {
@@ -19,7 +17,7 @@ class App extends Component {
         <h2>Counter</h2>
         <div className="counterDiv">
           <button onClick={this.decrement}>&ndash;</button>
-          <span className="count">{this.state.count}</span>
+          <span className="count">{this.props.count}</span>
           <button onClick={this.increment}>+</button>
         </div>
       </div>
@@ -27,4 +25,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({  // --- 2-й шаг определяем функцию mapStateToProps
+  count: state.count
+}); 
+
+export default connect(mapStateToProps)(App); //привязывает стейт к пропсам и оборачивает компонент
